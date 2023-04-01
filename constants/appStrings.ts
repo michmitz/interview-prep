7bb8d164a04363c6cd818aad0ce505d436a88b92
Michelle Stermitz <stermitzm@gmail.com>
2023-03-31 18:16:47 -0600

    Tweaks

--- constants/appStrings.ts
+++ constants/appStrings.ts
@@ -1,3 +1,10 @@
 export const appStrings = {
   prompt1: 'Pretend you are interviewing me for a frontend software position with 2 years of experience required. You are a polite interviewer. Ask me a question, then tell me how you would answer that question.',
 }
+
+
+// Todo: add prompts by experience level, interviewer difficulty, question types:
+// Experience levels: Bootcamp grad (junior dev), Mid-level, senior-level
+// Interviewer difficulty: kind, moderate, I don't want to work there
+// Q types: STAR (anecdotal), code challenges, 
+// Type of company

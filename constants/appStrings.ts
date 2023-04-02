81a3689b0155285dd4ba6dec4e4e492d3fe74f79
Michelle Stermitz <stermitzm@gmail.com>
2023-04-01 19:54:54 -0600

    minor error handling, change prompt

--- constants/appStrings.ts
+++ constants/appStrings.ts
@@ -1,5 +1,5 @@
 export const appStrings = {
-  prompt1: 'Pretend you are interviewing me for a frontend software position with 2 years of experience required. You are a polite interviewer. Ask me a question, then tell me how you would answer that question.',
+  prompt1: 'Pretend you are interviewing me for a frontend software position with 2 years of experience required. You are a polite interviewer. Ask me one question, then give me advice on how to answer that question.',
 }
 
 
--- pages/api/openai.ts
+++ pages/api/openai.ts
@@ -13,14 +13,18 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const completion = await openai.createCompletion({
-    model: 'text-davinci-003',
-    prompt: appStrings.prompt1,
-    temperature: 0.7,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0,
-    max_tokens: 256,
-  })
-  res.status(200).json({ result: completion.data })
+  try {
+    const completion = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt: appStrings.prompt1,
+      temperature: 0.7,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+      max_tokens: 256,
+    })
+    res.status(200).json({ result: completion.data })
+  } catch (err) {
+    console.log("ERROR:", err)
+  }
 }

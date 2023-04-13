0a1ffb522e1e305a055cb57503c79b440ec02d59
Michelle Stermitz <stermitzm@gmail.com>
2023-04-13 17:43:34 -0600

    Add icons package

--- components/question_card/QuestionCard.tsx
+++ components/question_card/QuestionCard.tsx
@@ -1,33 +1,36 @@
-import React from 'react'
-import styles from './QuestionCard.module.css'
+import { appStrings } from "@/constants/appStrings";
+import { WechatOutlined } from "@ant-design/icons";
+import React from "react";
+import styles from "./QuestionCard.module.css";
 
 interface QuestionCardProps {
-  readonly response: string
+  readonly response: string;
 }
 
+const { showAdviceText } = appStrings
 
 export const QuestionCard: React.FC<QuestionCardProps> = ({ response }) => {
-  const question = response.split('Answer:')[0]
-  const advice = response.split('Answer:')[1]
-  const [showAdvice, setShowAdvice] = React.useState<boolean>(false)
-  const { glassEffect } = styles
-  const questionStyles = `${glassEffect} ${styles.question}`
-  const adviceStyles = `${glassEffect} ${styles.advice}`
+  const question = response.split("Answer:")[0];
+  const advice = response.split("Answer:")[1];
+  const [showAdvice, setShowAdvice] = React.useState<boolean>(false);
+  const { glassEffect } = styles;
+  const questionStyles = `${glassEffect} ${styles.question}`;
+  const adviceStyles = `${glassEffect} ${styles.advice}`;
 
   const handleClick = () => {
-    setShowAdvice(true)
-  }
+    setShowAdvice(true);
+  };
 
   return (
     <>
       <div className={questionStyles}>
         {question}
-        <button onClick={handleClick}>Show Advice?</button>
+        <div className={styles.showAdvice}>
+          <WechatOutlined onClick={handleClick} />
+          <p>{showAdviceText}</p>
+        </div>
       </div>
-      {
-        showAdvice &&
-        <div className={adviceStyles}>{advice}</div>
-      }
+      {showAdvice && <div className={adviceStyles}>{advice}</div>}
     </>
-  )
-}
+  );
+};

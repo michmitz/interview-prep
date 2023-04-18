64b6f15c04db76279e17fd668391776ed0e000b6
Michelle Stermitz <stermitzm@gmail.com>
2023-04-18 16:41:03 -0600

    Ai generated subject dropdown container

---
+++ components/subject_dropdown/AISubjectDropdownContainer.tsx
@@ -0,0 +1,39 @@
+import { appStrings } from '@/constants/appStrings'
+import React from 'react'
+import { SubjectDropdown } from './SubjectDropdown'
+
+interface AISubjectDropdownContainerProps {
+  readonly onChange: (value: string) => void;
+}
+
+const { getSubjectsPrompt } = appStrings.aiPrompts
+
+export const AISubjectDropdownContainer: React.FC<AISubjectDropdownContainerProps> = ({ onChange }) => {
+  const [subjects, setSubjects] = React.useState<ReadonlyArray<string>>([])
+  const [defaultValue, setDefaultValue] = React.useState<string>('')
+
+  const fetchSubjects = async() => {
+    const response = await fetch("/api/openai", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ role: "user", content: getSubjectsPrompt, maxTokens: 400 }),
+    });
+    const data = await response.json();
+    const subjectsArr = data?.response?.content.split('\n')
+    setSubjects(subjectsArr);
+  }
+
+  React.useEffect(() => {
+    fetchSubjects()
+    setDefaultValue(subjects[0])
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+
+  return (
+    <>
+    {subjects.length ? <SubjectDropdown dropdownValues={subjects} defaultValue={defaultValue} onChange={onChange} /> : <>Loading</>}</>
+  )
+}

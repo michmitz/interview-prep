1fbfb05b403c67b3e0abf3f3fa5d8168b90e5060
Michelle Stermitz <stermitzm@gmail.com>
2023-04-20 15:42:39 -0600

    Create notes route

---
+++ pages/notes.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { NextPage } from 'next'
+import styles from "../styles/Home.module.css";
+
+const Notes: NextPage = () => {
+  return (
+    <div className={styles.main}>
+      Notes Page
+    </div>
+  )
+}
+
+export default Notes;

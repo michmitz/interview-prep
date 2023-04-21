7363f301fdd2143c809c22ca7dd3b128bac352e4
Michelle Stermitz <stermitzm@gmail.com>
2023-04-20 21:47:01 -0600

    Tweaks

--- pages/api/note/[id].ts
+++ pages/api/note/[id].ts
@@ -1,12 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import prisma from '../../../lib/prisma';
 
-export default async function handle(req: NextApiRequest, res: NextApiResponse, updatedNote: string) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method == "PUT") {
         const { id } = req.query;
+        const { note } = req.body;
         const post = await prisma.note.update({
             where: { id: String(id) },
-            data: { note: updatedNote },
+            data: { note: note },
         })
         return res.json(post);
     } else if (req.method == "DELETE") {
--- pages/api/note/index.ts
+++ pages/api/note/index.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import prisma from '../../../lib/prisma';
 
-export default async function handle(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method == "POST") {
         const { question, advice, note } = req.body
         const result = await prisma.note.create({

7d5c18d5064e0239b69d60ac3c7ec7e3b30b9205
Michelle Stermitz <stermitzm@gmail.com>
2023-04-21 00:06:46 -0600

    Get notes in notes page, log for now

--- pages/notes.tsx
+++ pages/notes.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
-import { NextPage } from 'next'
+import { GetServerSideProps, NextPage } from 'next'
 import styles from "../styles/Home.module.css";
+import prisma from '@/lib/prisma';
 
-const Notes: NextPage = () => {
+export const getServerSideProps: GetServerSideProps = async () => {
+
+  const notes = await prisma.note.findMany({});
+
+  return {
+      props: { notes },
+  }
+}
+
+interface NotesProps {
+  readonly notes: any
+}
+
+const Notes: NextPage<NotesProps> = ({ notes }) => {
+  console.log('notes', notes)
   return (
     <div className={styles.main}>
       Notes Page

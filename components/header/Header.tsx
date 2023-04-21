af5ad2db2560c3f2dd82e48215f84e53eb9373d8
Michelle Stermitz <stermitzm@gmail.com>
2023-04-21 16:04:41 -0600

    Add notes page navigation to header

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,8 +1,9 @@
 import { appStrings } from "@/constants/appStrings";
 import React from "react";
 import styles from "./HeaderStyles.module.css";
-import { SelectOutlined } from "@ant-design/icons";
+import { FormOutlined, SelectOutlined } from "@ant-design/icons";
 import { InterviewMode } from "@/pages";
+import { useRouter } from "next/router";
 
 export interface HeaderProps {
   readonly mode: InterviewMode;
@@ -12,13 +13,33 @@ export interface HeaderProps {
 const { header } = appStrings;
 
 export const Header: React.FC<HeaderProps> = ({ mode, onModeClick }) => {
+  const router = useRouter();
+
+  const handleNotesClick = () => {
+    router.push("/notes");
+  };
+
   return (
     <div className={styles.container}>
       <p>{header}</p>
-      <span className={styles.modeLabelContainer} onClick={() => onModeClick(mode)}>
-        <p className={styles.modeLabel}>Mode: {mode.toUpperCase()}</p>
-        <SelectOutlined className={styles.modeIcon} />
+
+      <div className={styles.labelContainer}>
+      <span
+        className={styles.labelContainer}
+        onClick={() => onModeClick(mode)}
+      >
+        <p className={styles.label}>Mode: {mode.toUpperCase()}</p>
+        <SelectOutlined />
       </span>
+
+      <span
+        className={styles.labelContainer}
+        onClick={() => handleNotesClick()}
+      >
+        <p className={styles.notesLabel}>View Notes</p>
+        <FormOutlined />
+      </span>
+      </div>
     </div>
   );
 };

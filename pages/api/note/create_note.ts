07fdb567f2e99ff78daac2bca1c7fb2878d76c8a
Michelle Stermitz <stermitzm@gmail.com>
2023-04-28 16:43:36 -0600

    Reformat

--- pages/api/note/create_note.ts
+++ pages/api/note/create_note.ts
@@ -1,23 +1,25 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import prisma from '../../../lib/prisma';
-import { getServerSession } from 'next-auth/next';
-import { authOptions } from "../auth/[...nextauth]"
+import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../../lib/prisma";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../auth/[...nextauth]";
 
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const session = await getServerSession(req, res, authOptions);
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const session = await getServerSession(req, res, authOptions)
+  if (req.method === "POST" && session) {
+    const { question, advice, note } = req.body;
 
-    if (req.method === "POST" && session) {
-        const { question, advice, note } = req.body
-
-        const result = await prisma.note.create({
-            data: {
-              question,
-              advice,
-              note,
-              author: { connect: { email: session?.user?.email as string } },
-            },
-        })
-        return res.json(result)
-    }
+    const result = await prisma.note.create({
+      data: {
+        question,
+        advice,
+        note,
+        author: { connect: { email: session?.user?.email as string } },
+      },
+    });
+    return res.json(result);
+  }
 }
--- pages/notes.tsx
+++ pages/notes.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import { GetServerSideProps, NextPage } from 'next'
+import React from "react";
+import { GetServerSideProps, NextPage } from "next";
 import styles from "../styles/Home.module.css";
-import prisma from '@/lib/prisma';
-import { Header } from '@/components/atoms/header/Header';
-import { appStrings } from '@/constants/appStrings';
-import { getSession, signIn, useSession } from 'next-auth/react';
+import prisma from "@/lib/prisma";
+import { Header } from "@/components/atoms/header/Header";
+import { appStrings } from "@/constants/appStrings";
+import { getSession, signIn, useSession } from "next-auth/react";
 
 export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
-  const session = await getSession({ req })
+  const session = await getSession({ req });
 
   if (!session) {
     res.statusCode = 403;
@@ -17,37 +17,38 @@ export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
   const notes = await prisma.note.findMany({
     where: {
       author: { email: session?.user?.email },
-    }
+    },
   });
 
   return {
-      props: { notes },
-  }
-}
+    props: { notes },
+  };
+};
 
 interface NotesProps {
-  readonly notes: any
+  readonly notes: any;
 }
 
-const { notesPage } = appStrings.header
+const { notesPage } = appStrings.header;
 
 const Notes: NextPage<NotesProps> = ({ notes }) => {
   const { data: session } = useSession();
 
-  console.log('notes', notes)
+  console.log("notes", notes);
   if (!session) {
     return (
-      <div>Not authorized to view this page
+      <div>
+        Not authorized to view this page
         <button onClick={() => signIn()}>Sign In</button>
       </div>
-    )
+    );
   }
   return (
     <div className={styles.main}>
       <Header headerText={notesPage} />
       Notes Page
     </div>
-  )
-}
+  );
+};
 
 export default Notes;

626201e537c0ddae19e99ee40b1ec724470b0b7c
Michelle Stermitz <stermitzm@gmail.com>
2023-04-27 19:24:27 -0600

    Temporarily update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import { Header } from "@/components/atoms/header/Header";
 import { SubjectField } from "@/components/atoms/subject_field/SubjectField";
 import { QuestionNotesSection } from "@/components/molecules/question_notes_section/QuestionNotesSection";
 import { LoadingOutlined } from "@ant-design/icons";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 const { askQuestionButton, thinking } = appStrings;
 const { askQuestionPrompt } = appStrings.aiPrompts;
@@ -14,21 +15,22 @@ const { welcome } = appStrings.header;
 export type InterviewMode = "subject" | "general";
 
 const Home: NextPage = () => {
+  const { data: session } = useSession();
   const [completion, setCompletion] = React.useState<string>("");
   const [questionLoading, setQuestionLoading] = React.useState<boolean>(false);
   const [mode, setMode] = React.useState<InterviewMode>("general");
   const [subject, setSubject] = React.useState<string>("JavaScript");
   const [noteResponse, setNoteResponse] = React.useState<string>("");
 
-  const disabledButtonStyles = `${styles.button} ${styles.disabledButton}`
+  const disabledButtonStyles = `${styles.button} ${styles.disabledButton}`;
 
   // const addNote = (note: any) => {
   //   setUpdatedNotes([...updatedNotes, note]);
   // };
 
   const handleClick = async (e: any) => {
-    setCompletion('')
-    setNoteResponse('')
+    setCompletion("");
+    setNoteResponse("");
     const content =
       mode === "subject"
         ? `${askQuestionPrompt} The interview subject is ${subject}.`
@@ -57,35 +59,58 @@ const Home: NextPage = () => {
     setSubject(value);
   };
 
+  if (session) {
+    return (
+      <div className={styles.main}>
+        <Header
+          headerText={welcome}
+          mode={mode}
+          onModeClick={handleModeClick}
+        />
+
+        <>
+          Signed in as {session?.user?.email} <br />
+          <button onClick={() => signOut()}>Sign out</button>
+        </>
+
+        <div className={styles.container}>
+          {mode === "subject" && (
+            <div>
+              <SubjectField onChange={onSubjectChange} />
+            </div>
+          )}
+          <button
+            onClick={handleClick}
+            className={questionLoading ? disabledButtonStyles : styles.button}
+            disabled={questionLoading}
+          >
+            {questionLoading ? <LoadingOutlined /> : askQuestionButton}
+          </button>
+          {questionLoading && (
+            <div className={styles.loading}>
+              <p className={styles.loadingText}>{thinking}</p>
+            </div>
+          )}
+          {completion && (
+            <>
+              <QuestionNotesSection
+                aiResponse={completion}
+                noteResponse={noteResponse}
+                setNoteResponse={setNoteResponse}
+              />
+            </>
+          )}
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className={styles.main}>
-      <Header headerText={welcome} mode={mode} onModeClick={handleModeClick} />
-
-      <div className={styles.container}>
-        {mode === "subject" && (
-          <div>
-            <SubjectField onChange={onSubjectChange} />
-          </div>
-        )}
-        <button
-          onClick={handleClick}
-          className={questionLoading ? disabledButtonStyles : styles.button}
-          disabled={questionLoading}
-        >
-          {questionLoading ? <LoadingOutlined /> : askQuestionButton}
-        </button>
-        {questionLoading && (
-          <div className={styles.loading}>
-            <p className={styles.loadingText}>{thinking}</p>
-          </div>
-        )}
-        {completion && (
-          <>
-            <QuestionNotesSection aiResponse={completion} noteResponse={noteResponse} setNoteResponse={setNoteResponse}
-             />
-          </>
-        )}
-      </div>
+      <>
+        Not signed in <br />
+        <button onClick={() => signIn()}>Sign in</button>
+      </>
     </div>
   );
 };

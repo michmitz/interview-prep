d90bf530e25664db915fd4fa39542ea5d5b4ed99
Michelle Stermitz <stermitzm@gmail.com>
2023-05-18 01:21:23 -0600

    Make subjectfield generic

--- components/atoms/subject_field/SubjectField.tsx
+++ components/atoms/subject_field/SubjectField.tsx
@@ -3,21 +3,23 @@ import React from "react";
 import styles from "./SubjectField.module.css";
 
 interface SubjectFieldProps {
+  readonly label: string;
   readonly onChange: (value: string) => void;
+  readonly placeholder: string;
 }
 
-const { jobTitleFieldLabel, jobTitleFieldPlaceholder } = appStrings.mode.jobTitle;
-
 export const SubjectField: React.FC<SubjectFieldProps> = ({
+  label,
   onChange,
+  placeholder,
 }) => {
   return (
     <div className={styles.container}>
-      <p className={styles.label}>{jobTitleFieldLabel}</p>
+      <p className={styles.label}>{label}</p>
       <input
         className={styles.input}
         onChange={e => onChange(e.target.value)}
-        placeholder={jobTitleFieldPlaceholder}
+        placeholder={placeholder}
       />
     </div>
   );

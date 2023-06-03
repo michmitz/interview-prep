e9fb26d6858c0340b82c89df893e562132980bcd
Michelle Stermitz <stermitzm@gmail.com>
2023-06-02 22:43:50 -0600

    Add verify page

---
+++ pages/auth/verify.tsx
@@ -0,0 +1,30 @@
+import { SpeechBubblePrompt } from "@/components/molecules/speech_bubble_prompt/SpeechBubblePrompt";
+
+const Verify = () => {
+  // useEffect(() => {
+  //   if (session) {
+  //     router.push("/");
+  //   }
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [session]);
+
+  return (
+    <div
+      style={{
+        backgroundImage: `url('/backgrounds/background-1.jpg')`,
+        height: "100vh",
+        width: "100vw",
+        backgroundSize: "cover",
+        overflow: "auto",
+        minHeight: "650px",
+      }}
+      className="centerContent"
+    >
+      <div className="signedOut">
+        <SpeechBubblePrompt text="Check your email for a link to sign in!" />
+      </div>
+    </div>
+  );
+};
+
+export default Verify;

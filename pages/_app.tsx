3a637288f5e5bd5a669a742b63c725579f37e5a2
Michelle Stermitz <stermitzm@gmail.com>
2023-06-04 13:05:07 -0600

    Add layout to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,50 +1,19 @@
 import React from "react";
 import "@/styles/globals.css";
-import { SessionProvider } from "next-auth/react";
+import { SessionProvider, getSession, useSession } from "next-auth/react";
 import type { AppProps } from "next/app";
 import type { Session } from "next-auth";
+import { Layout } from "@/components/molecules/layout/Layout";
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps<{ session: Session }>) {
-  const [randomBackground, setRandomBackground] = React.useState<string>("");
-
-  const backgrounds = [...Array(8).keys()].map((i) => {
-    return `background-${i + 1}.jpg`;
-  });
-
-  const randomizeBackground = () => {
-    const randomBgIndex = Math.floor(Math.random() * 9);
-    return backgrounds[randomBgIndex - 1];
-  };
-
-  const background = randomizeBackground();
-
-  React.useEffect(() => {
-    if (randomBackground) {
-      return;
-    }
-
-    setRandomBackground(background);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [randomBackground]);
-
   return (
     <SessionProvider session={session}>
-      <div
-        style={{
-          backgroundImage: `url('/${randomBackground}')`,
-          height: "100vh",
-          width: "100vw",
-          backgroundSize: "cover",
-          overflow: "auto",
-          minHeight: "650px",
-        }}
-        className="centerContent"
-      >
+      <Layout>
         <Component {...pageProps} />
-      </div>
+      </Layout>
     </SessionProvider>
   );
 }

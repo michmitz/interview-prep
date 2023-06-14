16350d5cb012f0ed984b3b6df36dfbf851ec5104
Michelle Stermitz <stermitzm@gmail.com>
2023-06-13 20:20:25 -0600

    Shelve random background for now

--- components/molecules/layout/Layout.tsx
+++ components/molecules/layout/Layout.tsx
@@ -8,39 +8,31 @@ interface LayoutProps {
 
 export const Layout = ({ children }: LayoutProps) => {
   const { data: session } = useSession();
-  const [randomBackground, setRandomBackground] =
-    React.useState<string>("background-1.jpg");
+  // const [randomBackground, setRandomBackground] =
+  //   React.useState<string>("background-1.jpg");
 
-  const backgrounds = [...Array(7).keys()].map((i) => {
-    return `background-${i + 1}.jpg`;
-  });
+  // const backgrounds = [...Array(7).keys()].map((i) => {
+  //   return `background-${i + 1}.jpg`;
+  // });
 
-  React.useEffect(() => {
-    if (randomBackground === "background-1.jpg" && session) {
-      const randomizeBackground = () => {
-        const randomBgIndex = Math.floor(Math.random() * 7);
-        return backgrounds[randomBgIndex];
-      };
-      const background = randomizeBackground();
-      setRandomBackground(background);
-    }
+  // React.useEffect(() => {
+  //   if (randomBackground === "background-1.jpg" && session) {
+  //     const randomizeBackground = () => {
+  //       const randomBgIndex = Math.floor(Math.random() * 7);
+  //       return backgrounds[randomBgIndex];
+  //     };
+  //     const background = randomizeBackground();
+  //     setRandomBackground(background);
+  //   }
 
-    if (randomBackground !== "background-1.jpg") {
-      return;
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [randomBackground, session]);
+  //   if (randomBackground !== "background-1.jpg") {
+  //     return;
+  //   }
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [randomBackground, session]);
 
   return (
     <div
-      // style={{
-      //   backgroundImage: `url('/${randomBackground}')`,
-      //   height: "100vh",
-      //   width: "100vw",
-      //   backgroundSize: "cover",
-      //   overflow: "auto",
-      //   backgroundPosition: "center center",
-      // }}
       style={{ width: '100vw', height: '100vh'}}
       className={`centerContent ${session && "fadeIn"}`}
     >
@@ -52,12 +44,10 @@ export const Layout = ({ children }: LayoutProps) => {
         sizes="100%"
         priority={true}
         quality={100}
-        // placeholder="blur"
-        // blurDataURL="/background-1.jpg"
         style={{
         objectFit: "cover",
         overflow: "auto",
-        backgroundPosition: "center center", position: 'absolute', zIndex: -10, }} // optional
+        backgroundPosition: "center center", position: 'absolute', zIndex: -100, }}
         alt="background"
       />
       {children}
